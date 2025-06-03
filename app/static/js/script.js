f6b79b140c23dae0ccc4271414d3f4d84b45172a
Shivam Bhundiya <145212542+Shivam-5403@users.noreply.github.com>
2025-06-03 10:33:12 +0530

    Update script.js

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -4,6 +4,7 @@ let snapshotInterval = null;
 function requestFullscreen() {
     const elem = document.documentElement;
     if (elem.requestFullscreen) elem.requestFullscreen();
+    else if (elem.mozRequestFullScreen) elem.mozRequestFullScreen();
     else if (elem.webkitRequestFullscreen) elem.webkitRequestFullscreen();
     else if (elem.msRequestFullscreen) elem.msRequestFullscreen();
 }
